52b3127f938516bb29f79d92bbf655a9c31b5cb1
Marcos Albuquerque <mpdsa@pm.me>
2023-06-01 12:32:40 -0300

    Componente <app-menu-bar>

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css']
+  styleUrls: ['./menu-bar.component.sass']
 })
 export class MenuBarComponent {
 
