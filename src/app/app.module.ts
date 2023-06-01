e42f086adf9a850c5958bb7de32944ed38ff5025
Marcos Albuquerque <mpdsa@pm.me>
2023-06-01 12:37:01 -0300

    Ativado componente: Home,Notice,NotiveOverview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { PostsComponent } from './components/posts/posts.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NoticeComponent } from './pages/notice/notice.component';
+import { NoticeOverviewComponent } from './components/notice-overview/notice-overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
-    PostsComponent
+    PostsComponent,
+    HomeComponent,
+    NoticeComponent,
+    NoticeOverviewComponent
   ],
   imports: [
     BrowserModule,
