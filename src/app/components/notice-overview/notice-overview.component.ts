5247e239cc74e8c9469856a699e76fd15e00075e
Marcos Albuquerque <mpdsa@pm.me>
2023-06-01 12:12:54 -0300

    Componente para exibir na página inicial, noticias recentes

---
+++ src/app/components/notice-overview/notice-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-notice-overview',
+  styleUrls: ['./notice-overview.component.sass'],
+  templateUrl: './notice-overview.component.html'
+})
+
+
+export class NoticeOverviewComponent { }
