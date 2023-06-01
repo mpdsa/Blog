c52ade73b7a6ecea13fa7a4b382fab6d805f3209
Marcos Albuquerque <mpdsa@pm.me>
2023-06-01 12:39:59 -0300

    Rota: '/noticia' implementada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NoticeComponent } from './pages/notice/notice.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
+  path: 'noticia',
+  component: NoticeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
